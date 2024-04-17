22da121e0b6b541441bd92e1458dca553ec43bf5
Lisa <82220848+lisaviktorsson@users.noreply.github.com>
2024-04-17 12:29:28 +0200

    testing folder

--- docs/_app/immutable/entry/start.C15Ch7VM.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CjZdL4Ug.js";export{t as start};
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,9 @@ import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin'
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-    // Example content paths...
-    './docs/*.html',
-    './src/**/*.{js,jsx,ts,tsx,svelte}',
-  ],
+	
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: ['./src/**/*.{html,js,svelte,ts}','./docs/*.html' , join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
 		extend: {},
 	},
