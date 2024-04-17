b61102910e321ebc8b671f3d9deec61848923054
Lisa <82220848+lisaviktorsson@users.noreply.github.com>
2024-04-17 11:40:28 +0200

    removing stuff

---
+++ docs/_app/immutable/entry/start.ByRTQcW_.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DoU3ZOIK.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+import remarkGithub from 'remark-github';
+import remarkAbbr from 'remark-abbr';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), ],
+	preprocess: [preprocess(), vitePreprocess(), ],
 
 	kit: {
 		adapter: adapter({
@@ -15,8 +21,12 @@ const config = {
 		}),
 		
 		
-	}
+	},prerender: {
+			default: true
+	},
+	
 
+	extensions: ['.svelte'],
 	
 
 };
